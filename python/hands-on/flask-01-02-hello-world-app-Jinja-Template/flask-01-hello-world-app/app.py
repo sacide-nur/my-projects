f315b1a2807b623a738b2fe7ca80afdc063b541b
Sacide-NUr <sacideakkoyu@gmail.com>
2022-01-30 09:55:58 -0800

    Project-101 created

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/app.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/app.py
@@ -4,19 +4,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def head():
-    return "Hello World!"
+    return "<h1>You are gonna be a good DevOps and AWS Solution Architech</h1>"
 
-
-
-@app.route('/second')
+@app.route("/second")
 def second():
-    return "Gonuller Bir Olsun"
-@app.route('/third/subthird')
+    return "This is the second page"
+
+@app.route("/third/tunc")
 def third():
-    return 'This is the subpage of third page' 
-@app.route('/forth/<string:id>')
+    return "This is the subpath of third path"
+
+@app.route("/forth/<string:id>")
 def forth(id):
-    return f'Id number of this page is {id}'
+    return f'Id of this page is {id}'
 
 if __name__=="__main__":
-    app.run(debug=True, port=2000)   
+    app.run(debug=True)
+    
